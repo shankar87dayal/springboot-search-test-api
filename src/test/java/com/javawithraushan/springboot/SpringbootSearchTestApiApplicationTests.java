87fb90b5fa27cc40be58834207889717dae80f48
shankar87dayal <shankar.87.dayal@gmail.com>
2022-12-30 06:33:51 +0530

    init commit

---
+++ src/test/java/com/javawithraushan/springboot/SpringbootSearchTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javawithraushan.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSearchTestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
